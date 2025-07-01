de1ef119c433c994cbdb18fd9433247326a8edfd
Admin <mfbfaisal@gmai.com>
2025-07-01 22:48:10 +0600

    first commit next js fullstack project

---
+++ src/libs/ulinterface.ts
@@ -0,0 +1,30 @@
+// uiinterface.ts
+export interface UiUser {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export const VIDEO_DIMENSION = {
+  width: 1080,
+  height: 1920,
+} as const;
+
+export interface UiVideo {
+  id: string;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  controls?:boolean;
+  transformtion: { 
+    height: number;
+    width: number;
+    quality?: number;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
